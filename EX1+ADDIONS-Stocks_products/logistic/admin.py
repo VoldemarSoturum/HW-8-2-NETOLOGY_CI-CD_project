fe5faf06843a1f13ee1dbd3213b50dd0b3d6f6e3
Voldemar_Stoturum <ivangoe110692@gmail.com>
2025-12-08 23:36:39 +0300

    Fix flake8 F401/F841 and relax line-length in CI

--- EX1+ADDIONS-Stocks_products/logistic/admin.py
+++ EX1+ADDIONS-Stocks_products/logistic/admin.py
@@ -1,8 +1,19 @@
 from django.contrib import admin
 from .models import Product, Stock, StockProduct
 
-admin.site.register(Product)
-admin.site.register(Stock)
-admin.site.register(StockProduct)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "description")
+
+
+@admin.register(Stock)
+class StockAdmin(admin.ModelAdmin):
+    list_display = ("id", "address")
+
+
+@admin.register(StockProduct)
+class StockProductAdmin(admin.ModelAdmin):
+    list_display = ("id", "stock", "product", "quantity", "price")
 
 # Register your models here.

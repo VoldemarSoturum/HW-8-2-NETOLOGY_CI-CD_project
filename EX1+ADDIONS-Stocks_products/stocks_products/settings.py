eeff730e336a6b499c22875cc936c7f2483c114f
Voldemar_Stoturum <ivangoe110692@gmail.com>
2025-12-08 22:06:19 +0300

    Black-ed settings.py

--- EX1+ADDIONS-Stocks_products/stocks_products/settings.py
+++ EX1+ADDIONS-Stocks_products/stocks_products/settings.py
@@ -168,5 +168,5 @@ REST_FRAMEWORK = {
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 # Настройки медиа файлов (если нужны)
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
